656679eeaf070ad32e6657b3ed7b8463fb89e068
Yuri Simeone <simeoneyuri@gmail.com>
2021-01-02 16:16:14 +0100

    Prima versione di test

---
+++ bin/index-global.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const aemClientlibsCopier = require('../src/index.js')
+
+aemClientlibsCopier.start()
---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+const start = config => {
+    // controllo che config sia valido
+    if (config) {
+
+    }
+    
+}
+
+
+
+
+exports.start = start;
